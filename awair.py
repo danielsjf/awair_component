b94164eaccee48e8162da44dae2c48687d3a446a
danielsjf <jef.daniels1@gmail.com>
2018-09-24 00:12:31 +0200

    Update to version 0.0.11

--- awair.py
+++ awair.py
@@ -13,7 +13,7 @@ from homeassistant.helpers.entity import Entity
 
 CONF_REFRESH = "refresh_rate"
 
-REQUIREMENTS = ['pyawair==0.0.9']
+REQUIREMENTS = ['pyawair==0.0.11']
 
 _LOGGER = logging.getLogger(__name__)
 
